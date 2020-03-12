af98261e3d70555170fd2cf8cfff5e546fa1cf24
Gautier P <gautier+git@damsy.net>
2020-03-12 20:06:53 +0100

    Remove old leaflet property

--- leaflet-triangle-marker/leaflet-triangle-marker.js
+++ leaflet-triangle-marker/leaflet-triangle-marker.js
@@ -20,8 +20,6 @@ L.Canvas.prototype._updateTriangle = function (layer) {
     // rotation angle from deg to rad
     r   = layer._rotation !== 0 ? layer._rotation * (Math.PI * 2) / 360 : 0
 
-    this._drawnLayers[layer._leaflet_id] = layer
-
     // rotate canvas on triangle center before drawing
     ctx.save()
     ctx.translate(p.x, p.y)
